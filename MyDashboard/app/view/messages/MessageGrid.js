db6a4b26cd5583aac0f62ed47fea0d7523db556f
Winniekimani <kimaniwinniew@gmail.com>
2024-03-31 01:32:12 +0300

    Started mail client  	-created models and stores for contacts labels and messages. 	-made message grid view to display all of the loaded messages 	-created messagereader to display full details of a message

--- MyDashboard/app/view/messages/MessageGrid.js
+++ MyDashboard/app/view/messages/MessageGrid.js
@@ -14,27 +14,78 @@ Ext.define('MyDahboard.view.messages.MessageGrid', {
     
 
 
+    listeners: {
+        beforeselect: function() {
+            return false;
+        }
+    },
+
     columns: [
+        {
+            xtype: 'actioncolumn',
+            width: 25,
+            items: [
+                {
+                    glyph: '☆',
+                    tooltip: 'Star',
+                    handler: function(view, rowIndex, colIndex, item, e, rec) {
+                        view.grid.fireEvent('starmessage', rec);
+                    },                                                          
+                    getClass: function(v, meta, rec) {   
+                        if(rec.get('starred')) {                                                                      
+                            return 'x-hidden-display';
+                        }
+                    }
+                },
+                {
+                    glyph: '★',
+                    tooltip: 'Un-star',
+                    handler: function(view, rowIndex, colIndex, item, e, rec) {
+                        view.grid.fireEvent('unstarmessage', rec);
+                    },                                                          
+                    getClass: function(v, meta, rec) {          
+                        if(!rec.get('starred')) {                                                                      
+                            return 'x-hidden-display';
+                        }
+                    }
+                }
+            ]
+        },
         {
             dataIndex: 'fullName',
             minWidth: 250,
-            header: false, // Keep it as is
-            tdCls: 'full-name' // Keep it as is
+            header: false,
+            tdCls: 'full-name',
+            renderer: function(value, meta, rec) {
+
+                if (rec.get('draft')) {
+                    meta.tdStyle += 'color: red;'
+                    return 'Draft';
+                }
+
+                if (rec.get('outgoing') && rec.get('sent')) {
+                    return 'To: ' + value;
+                }
+
+                return value;
+            }
         },
         {
             dataIndex: 'subject',
             flex: 1,
-            header: false // Keep it as is
+            header: false,
+            renderer: function(value) {               
+                return Ext.isEmpty(value) ? '(no subject)' : value;
+            }
         },
         {
             xtype: 'datecolumn',
             dataIndex: 'date',
-            header: 'Received', // Keep it as is
+            header: 'Received',
             width: 100,
-            header: false, // Keep it as is
-            align: 'end', // Keep it as is
-            format: 'j M \'y' // Keep it as is
+            header: false,
+            align: 'end',
+            format: 'j M \'y'
         }
     ]
-    
 });
